1947ecaa05c2839f163eaeec28fb281ceb8c427a
Joris Lefever <joris.lefever@ordina.be>
2017-01-10 16:19:42 +0100

    Changing the config files and place of docker-compose file. With these properties files, the services can be started correctly. The docker-compose file is still not working.

--- user-service/src/main/java/com/homegrown/UserServiceApplication.java
+++ user-service/src/main/java/com/homegrown/UserServiceApplication.java
@@ -1,20 +1,11 @@
 package com.homegrown;
 
-import com.homegrown.entity.User;
-import com.homegrown.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.integration.annotation.IntegrationComponentScan;
-import org.springframework.integration.annotation.MessageEndpoint;
-import org.springframework.integration.annotation.ServiceActivator;
 
 @EnableDiscoveryClient
-@EnableBinding(Sink.class)
 @SpringBootApplication
-@IntegrationComponentScan
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
