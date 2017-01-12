ec6771df254147a4651ba46a7a9e2e6aa2befcf7
Joris Lefever <joris.lefever@ordina.be>
2017-01-12 09:42:43 +0100

    Adding new config

--- users-microservice/src/main/java/com/homegrown/UserServiceApplication.java
+++ users-microservice/src/main/java/com/homegrown/UserServiceApplication.java
@@ -3,15 +3,42 @@ package com.homegrown;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableRetry
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(UserServiceApplication.class, args);
 
+	}
+
+	/**
+	 * This method creates a retry policy for when using the docker compose.
+	 * When the config server is not yet running, this will retry 5 times
+	 *
+	 * @return
+	 */
+	@Bean
+	public RetryTemplate retryTemplate() {
+		SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+		retryPolicy.setMaxAttempts(5);
+
+		FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+		backOffPolicy.setBackOffPeriod(1500); // 1.5 seconds
+
+		RetryTemplate template = new RetryTemplate();
+		template.setRetryPolicy(retryPolicy);
+		template.setBackOffPolicy(backOffPolicy);
+
+		return template;
 	}
 
 }
